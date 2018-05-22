ac3abd967b5a716d3b78f92e473b90f0625be6fd
wonderboy <wonderboy39@outlook.com>
2018-05-22 20:03:14 +0900

    집에가기전 중간 커밋...

--- boardTemplate/src/main/java/com/spring/scrapper/board/video/VideoBoardController.java
+++ boardTemplate/src/main/java/com/spring/scrapper/board/video/VideoBoardController.java
@@ -1,5 +1,7 @@
 package com.spring.scrapper.board.video;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -11,32 +13,51 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.scrapper.board.video.vo.VideoBoardVO;
+
 @Controller
 public class VideoBoardController {
 	@Inject
 	private SqlSession sqlSession;
 	
+	@Inject
+	private VideoBoardService videoService;
+	
 	@RequestMapping(value="/video/list",  method=RequestMethod.GET)
 	public ModelAndView getBoardList(){
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/scrapper/video/list");
 		// service -> dao
+		List<VideoBoardVO> videoList = null;
+		try {
+			videoList = videoService.getBoardList();
+			mav.setViewName("/scrapper/video/list");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return mav;
 	}
 	
 	@RequestMapping(value="/video", method=RequestMethod.GET)
 	public ModelAndView getBoard(@RequestParam(required=false) int postId){
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/scrapper/video/list");
+		mav.setViewName("/scrapper/video/board");
 		// postId...
 		// service -> dao
+		return mav;
+	}
+	
+	@RequestMapping(value="/video", method=RequestMethod.GET)
+	public ModelAndView modifyBoardGet(@PathVariable int postId, Model Model){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/scrapper/video/updateBoard");
 		return mav;
 	}
 	
 	@RequestMapping(value="/video", method=RequestMethod.PUT)
 	public ModelAndView modifyBoard(@PathVariable int postId, Model model){
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/scrapper/video/list");
+		mav.setViewName("redirect:/scrapper/video/board");
 		// service -> dao
 		return mav;
 	}
@@ -44,7 +65,7 @@ public class VideoBoardController {
 	@RequestMapping(value="/video", method=RequestMethod.POST)
 	public ModelAndView createBoard(@PathVariable int postId){
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/scrapper/video/list");
+		mav.setViewName("redirect:/scrapper/video/list");
 		// service -> dao
 		return mav;
 	}
@@ -52,7 +73,7 @@ public class VideoBoardController {
 	@RequestMapping(value="/video", method=RequestMethod.DELETE)
 	public ModelAndView deleteBoard(@PathVariable int postId){
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/scrapper/video/list");
+		mav.setViewName("redirect:/scrapper/video/list");
 		// service -> dao
 		return mav;
 	}
